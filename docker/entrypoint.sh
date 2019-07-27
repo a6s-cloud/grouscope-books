7e41f7618e25b054c21becd595a78bd3b4409a3d
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-07-27 21:06:11 +0900

    Re:VIEW ファイルを監視するスクリプトを追加

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,52 +2,61 @@
 
 WORK_DIR="/root/grouscope-books"
 DESTINATION="mybook.pdf"
+EMOJI_POPPER="🎉"
+EMOJI_SORROWLY="😱"
+EMOJI_GLASS="🧐"
 
 main() {
 
     cd "$WORK_DIR"
-    if [[ ! -f "$DESTINATION" ]]; then
-        rake pdf
-        echo "$(date +%Y%m%d %H:%M:%S) 初期ファイルを作成しました"
-    fi
 
-    declare -A last_files_of_stats
+    rake pdf
+    echo "$(date "+%Y/%m/%d %H:%M:%S") INFO: ${EMOJI_POPPER}初期ファイルを作成しました${EMOJI_POPPER} [File: ${DESTINATION}]"
+    echo "$(date "+%Y/%m/%d %H:%M:%S") INFO: モニタリングを開始しました${EMOJI_GLASS}。Re:VIEW ファイルの追加/削除/変更を監視して自動でPDF ファイルを出力します"
 
     # ファイルキャッシュリストを作成する
+    declare -A last_files_of_stats
     local file
+    local hash_value
     while read file; do
-        last_files_of_stats=true
+        hash_value="$(sha1sum "$file" 2> /dev/null | cut -d ' ' -f 1)"
+        last_files_of_stats["$file"]="$hash_value"
     done < <(find . -name "*.re" -type f | sed -e 's|^\./||')
 
     local path
     local action
-    local file
-    local hash_value
     local last_hash_value
     local result_of_rake
-    inotifywait -m /root/grouscope-books -e create -e move -e delete |
-            while read path action file; do
-                if [[ "$file" =~ .*\.re$ ]]; then
-                    if [[ "$action" == "MOVED_FROM" ]] || [[ "$action" == "DELETE" ]]; then
-                        [[ ! -f "$file" ]] && unset last_files_of_stats["$file"]
-                    fi
 
-                    hash_value="$(sha1sum "$file" | cut -d ' ' -f 1)"
-                    #echo "Last_hash: ${last_files_of_stats["$file"]}, Current_hash: $hash_value, File: $file"
-                    if [[ "${last_files_of_stats["$file"]}" != "$hash_value" ]]; then
-                        echo "##### rake pdf -- File: $file, Action: $action"
-                        last_hash_value="${last_files_of_stats["$file"]}"
-                        last_files_of_stats["$file"]="$hash_value"
-                        rake pdf
-                        result_of_rake=$?
-                        if [[ $result_of_rake -eq 0 ]]; then
-                            echo "INFO: ビルドが成功しました[トリガ: File: $file, Action: $action, last_hash: ${last_hash_value:0:7}, current_hash: ${hash_value:0:7}]"
-                        else
-                            echo "ERROR: ビルドに失敗しました[トリガ: File: $file, Action: $action, last_hash: ${last_hash_value:0:7}, current_hash: ${hash_value:0:7}]" >&2
-                        fi
-                    fi
+    while read path action file; do
+        # echo "File: $file, Action: $action"    # debug
+        if [[ "$file" =~ .*\.re$ ]]; then
+            last_hash_value="${last_files_of_stats["$file"]}"
+            hash_value="$(sha1sum "$file" 2> /dev/null | cut -d ' ' -f 1)"
+
+            if [[ "$action" == "MOVED_FROM" ]] || [[ "$action" == "DELETE" ]]; then
+                [[ ! -f "$file" ]] && unset last_files_of_stats["$file"]
+            fi
+
+            # echo "Last_hash: $last_hash_value, Current_hash: $hash_value, File: $file"    # debug
+            if [[ "$last_hash_value" != "$hash_value" ]]; then
+                if [[ ! -z "$hash_value" ]]; then
+                    last_files_of_stats["$file"]="$hash_value"
                 fi
-            done
+
+                echo "$(date "+%Y/%m/%d %H:%M:%S") INFO: rake pdf -- File: $file, Action: $action, Hash: ${hash_value:0:7}"
+                rake pdf
+                result_of_rake=$?
+                echo "============================================================================="
+                if [[ $result_of_rake -eq 0 ]]; then
+                    echo "$(date "+%Y/%m/%d %H:%M:%S") INFO: ${EMOJI_POPPER}ビルドが成功しました${EMOJI_POPPER}[トリガ: File: $file, Action: $action, last_hash: ${last_hash_value:0:7}, current_hash: ${hash_value:0:7}, out: mybook.pdf]"
+                else
+                    echo "$(date "+%Y/%m/%d %H:%M:%S") ERROR: ${EMOJI_SORROWLY}ビルドに失敗しました${EMOJI_SORROWLY}[トリガ: File: $file, Action: $action, last_hash: ${last_hash_value:0:7}, current_hash: ${hash_value:0:7}]" >&2
+                fi
+                echo "============================================================================="
+            fi
+        fi
+    done < <(inotifywait -m /root/grouscope-books -e create -e move -e delete)
 }
 
 main "$@"
---
+++ watch.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+IMAGE="a6scloud/grouscope-books"
+TAG="latest"
+#TAG="testing"
+
+main() {
+    local opt="$1"
+    local script_dir=
+
+    # このスクリプトがあるディレクトリに移動する
+    cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+    if [[ "$opt" == "-d" ]]; then
+        docker run --rm --name grouscope_books \
+                --volume "${PWD}:/root/grouscope-books" -d ${IMAGE}:${TAG}
+    else
+        docker run --rm --name grouscope_books \
+                --volume "${PWD}:/root/grouscope-books" -ti ${IMAGE}:${TAG}
+    fi
+}
+
+main "$@"
